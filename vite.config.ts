5cfe23ff27cd6023bc3aa5b2b99cd78c4c984112
Victor Nogueira <felladrin@gmail.com>
2022-01-20 19:09:39 +0000

    Inject build info at build time instead of changing the package version when main branch is updated
    This prevents Cloudflare from doing unnecessary builds and makes it easier to find the specific commit from the build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import hash from "helper-git-hash";
 
 export default defineConfig({
   base: "./",
@@ -8,6 +9,10 @@ export default defineConfig({
     alias: {
       winbox: "winbox/src/js/winbox.js",
     },
+  },
+  define: {
+    "process.env.BUILD_DATE_TIME": Date.now(),
+    "process.env.COMMIT_SHORT_HASH": JSON.stringify(hash({ short: true })),
   },
   server: {
     hmr: {
