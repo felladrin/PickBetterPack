4f21729c5bfd38b6f41e7721df61c696782f083b
Victor Nogueira <felladrin@gmail.com>
2022-06-05 00:36:26 +0000

    Fix typings

--- src/components/LanguageSelectionModal.svelte
+++ src/components/LanguageSelectionModal.svelte
@@ -1,6 +1,9 @@
-<script>
+<script type="ts">
   import { locale, locales, t } from "svelte-i18n";
-  import ISO6391 from "iso-639-1";
+  import ISO6391, { type LanguageCode } from "iso-639-1";
+  import type { Readable } from "svelte/store";
+
+  const langugeCodes = locales as Readable<LanguageCode[]>;
 </script>
 
 <div class="modal" id="modal-language" tabindex="-1" role="dialog">
@@ -8,7 +11,7 @@
     <div class="modal-content">
       <h5 class="modal-title">{$t("languageSelection")}</h5>
       <select class="form-control" bind:value={$locale}>
-        {#each $locales as localeCode}
+        {#each $langugeCodes as localeCode}
           <option value={localeCode}>
             {ISO6391.getNativeName(localeCode)}
           </option>
