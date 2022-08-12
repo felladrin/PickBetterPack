914393813861cf1b5b12c760e631f85901684756
Victor Nogueira <felladrin@gmail.com>
2022-08-12 10:21:35 +0000

    Update Vite config for Gitpod

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import hash from "helper-git-hash";
+import { execSync } from "node:child_process";
 
 export default defineConfig({
   base: "./",
@@ -14,9 +15,19 @@ export default defineConfig({
     "process.env.BUILD_DATE_TIME": Date.now(),
     "process.env.COMMIT_SHORT_HASH": JSON.stringify(hash({ short: true })),
   },
-  server: {
-    hmr: {
-      clientPort: process.env.GITPOD_WORKSPACE_ID ? 443 : undefined,
-    },
-  },
+  server: (() => {
+    try {
+      const port = 5173;
+      const gitpodPortUrl = execSync(`gp url ${port}`).toString().trim();
+      return {
+        strictPort: true,
+        port,
+        hmr: {
+          protocol: "wss",
+          host: new URL(gitpodPortUrl).hostname,
+          clientPort: 443,
+        },
+      };
+    } catch {}
+  })(),
 });
