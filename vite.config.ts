ce3c982d787ef92ddc998ea017a6abf22a0634f8
Victor Nogueira <felladrin@gmail.com>
2023-04-07 03:40:50 +0300

    Add docker-compose configuration (#323)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import hash from "helper-git-hash";
-import { execSync } from "node:child_process";
 
 export default defineConfig({
   base: "./",
@@ -15,19 +14,4 @@ export default defineConfig({
     "process.env.BUILD_DATE_TIME": Date.now(),
     "process.env.COMMIT_SHORT_HASH": JSON.stringify(hash({ short: true })),
   },
-  server: (() => {
-    try {
-      const port = 5173;
-      const gitpodPortUrl = execSync(`gp url ${port}`).toString().trim();
-      return {
-        strictPort: true,
-        port,
-        hmr: {
-          protocol: "wss",
-          host: new URL(gitpodPortUrl).hostname,
-          clientPort: 443,
-        },
-      };
-    } catch {}
-  })(),
 });
