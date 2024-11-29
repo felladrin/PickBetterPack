b57874f03533a5d2286516931bc3e6f5b95eec57
Victor Nogueira <felladrin@gmail.com>
2024-11-29 14:10:52 +0100

    Remove `executeAfterPolyfill` function, as https://polyfill.io doesn't exist anymore

--- src/functions/executeAfterPolyfill.ts
+++
@@ -1,5 +0,0 @@
-import { loadScript } from "./loadScript";
-
-export function executeAfterPolyfill(callback: () => void) {
-  loadScript("https://polyfill.io/v3/polyfill.min.js", callback);
-}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import App from "./components/App.svelte";
 import halfmoon from "halfmoon";
-import { executeAfterPolyfill } from "./functions/executeAfterPolyfill";
 import { loadLocale } from "./functions/loadLocale";
 import { mount } from "svelte";
 
-executeAfterPolyfill(async () => {
+(async () => {
   await loadLocale();
   mount(App, { target: document.getElementById("app") });
   halfmoon.onDOMContentLoaded();
-});
+})();
